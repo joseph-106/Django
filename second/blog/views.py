68e50b1b42b6a7a3971798fdbeddd150c8f48076
unknown <sjjoseph106@gmail.com>
2021-07-14 21:12:08 +0900

    Queryset & Method

--- second/blog/views.py
+++ second/blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Blog
 
-# Create your views here.
+def home(request):
+    blogs = Blog.objects #쿼리셋 -> 모델의 Blog 객체 목록을 변수에 저장
+    return render(request, 'home.html', {'blogs': blogs})
