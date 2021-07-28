34c91b288816ec0122985134257b120c86f7adef
unknown <sjjoseph106@gmail.com>
2021-07-28 13:52:39 +0900

    Pagination 실습

--- second/blog/views.py
+++ second/blog/views.py
@@ -1,10 +1,20 @@
+from django.core import paginator
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
+from django.core.paginator import Paginator
 from .models import Blog
 
 def home(request):
-    blogs = Blog.objects #쿼리셋 -> 모델의 Blog 객체 목록을 변수에 저장
-    return render(request, 'home.html', {'blogs': blogs})
+    blogs = Blog.objects
+    #블로그 모드 글들을 대상으로
+    blog_list = Blog.objects.all()
+    #블로그 객체 3개를 한 페이지로 자르기
+    paginator = Paginator(blog_list, 3)
+    #request된 페이지가 뭔지를 알아내고 (request 페이지를 변수에 담아내고)
+    page = request.GET.get('page')
+    #request된 페이지를 얻어온 뒤 return 해 준다
+    posts = paginator.get_page(page)
+    return render(request, 'home.html', {'blogs': blogs, 'posts':posts})
 
 def detail(request, blog_id):
     blog_detail = get_object_or_404(Blog, pk= blog_id) #이용자가 원한 몇 번 블로그 객체
